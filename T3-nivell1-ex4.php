45855644729e978b2d94451b2836913203418a45
clara-cdp <clarianne.cdp@gmail.com>
2025-12-01 13:59:09 +0100

    nivell1-ex4 fet

---
+++ T3-nivell1-ex4.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h3> * * *TASCA-3 * NIVELL 1 * Exercici 4 * * * <br></h3>\n";
+/*Fes un array associatiu que representi informació de tu mateix/a. En concret ha d’incloure:
+nom, edat, email, menjar favorit */
+
+$arrayMe = ["nom" => "Clara CDP", "edat" => "29", "email" => "myemail@gmail.com", "menajar favorit" => "french fries"];
