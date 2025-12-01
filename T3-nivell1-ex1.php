124b7d231df9370b861133b620830a63944d6646
clara-cdp <clarianne.cdp@gmail.com>
2025-12-01 12:05:35 +0100

    nivell1-ex1 fet

---
+++ T3-nivell1-ex1.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h3> * * *TASCA-3 * NIVELL 1 * Exercici 1 * * * <br></h3>\n";
+
+//Crea un array, afegeix-li 5 nombres enters i després mostrals per pantalla d’un en un.
+
+$numArray = array(300, 5, 42, 16, 82);
+
+foreach ($numArray as $x) {
+    echo $x . "\n";
+}
