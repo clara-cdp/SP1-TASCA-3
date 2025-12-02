5f8116361125c6fd180c9efff0046fba184aeeb7
clara-cdp <clarianne.cdp@gmail.com>
2025-12-02 11:14:57 +0100

    nivell2 - completat

--- T3-nivell1-ex4.php
+++ T3-nivell1-ex4.php
@@ -3,4 +3,4 @@ echo "<h3> * * *TASCA-3 * NIVELL 1 * Exercici 4 * * * <br></h3>\n";
 /*Fes un array associatiu que representi informació de tu mateix/a. En concret ha d’incloure:
 nom, edat, email, menjar favorit */
 
-$arrayMe = ["nom" => "Clara CDP", "edat" => "29", "email" => "myemail@gmail.com", "menajar favorit" => "french fries"];
+$arrayMe = ["nom" => "Clara CDP", "edat" => "29", "email" => "myemail@gmail.com", "menjar favorit" => "french fries"];
---
+++ T3-nivell2-ex1.php
@@ -0,0 +1,39 @@
+<?php
+echo "<h3> * * *TASCA-3 * NIVELL 2 * Exercici 1 * * * <br></h3>\n";
+/*Imagina que tens dues llistes de convidats(representats/es únicament per noms). Fes un programa que et retorni:*/
+
+$list1 = ["Arale", "Gatchan", "Akane", "Pisuke", "Taro", "Sembei"];
+$list2 = ["Ranma", "Akane", "Xampu", "Ryoga", "Taro"];
+
+#La llista de convidats en comú entre les dues llistes.
+$commonGuests = array_intersect($list1, $list2);
+//var_dump($commonGuests);
+echo "Convidats en comú: " . implode(", ", $commonGuests) . "\n";
+
+#La mescla de la llista de convidats(sense repeticions).
+$newList = array_merge($list1, $list2);
+$newList = array_unique($newList);
+//var_dump($newList);
+echo "Convidats, sense repeticions: " . implode(", ", $newList) . "\n";
+
+#La llista de convidats exclusius de la primera llista.
+$exclusiveList1 = [];
+
+foreach ($list1 as $guest) {
+    if (!in_array($guest, $list2)) {
+        $exclusiveList1[] = $guest;
+    }
+}
+
+echo "Convidats exclusius de la llista-1: " . implode(", ", $exclusiveList1) . "\n";
+
+#La llista de convidats exclusius de la segona llista.
+$exclusiveList2 = [];
+
+foreach ($list2 as $guest) {
+    if (!in_array($guest, $list1)) {
+        $exclusiveList2[] = $guest;
+    }
+}
+
+echo "Convidats exclusious de la llista-2: " . implode(", ", $exclusiveList2);
