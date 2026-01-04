7a0bc69faa1135c0b988b15e3fadafdbe34937f5
clara-cdp <clarianne.cdp@gmail.com>
2026-01-04 18:20:26 +0100

    updated code Nivell2

--- T3-nivell2-ex1.php
+++ T3-nivell2-ex1.php
@@ -1,40 +1,135 @@
-<?php
-echo "<h3> * * *TASCA-3 * NIVELL 2 * Exercici 1 * * * <br></h3>\n";
-/*Imagina que tens dues llistes de convidats(representats/es únicament per noms). Fes un programa que et retorni:*/
+<!DOCTYPE html>
+<html lang="en">
 
-$list1 = ["Arale", "Gatchan", "Akane", "Pisuke", "Taro", "Sembei"];
-$list2 = ["Ranma", "Akane", "Xampu", "Ryoga", "Taro"];
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>T3-N2-EX1</title>
+</head>
 
-#La llista de convidats en comú entre les dues llistes.
-$commonGuests = array_intersect($list1, $list2);
-echo "Convidats en comú: " . implode(", ", $commonGuests) . "\n";
+<body>
+    <h3> * * *TASCA-3 * NIVELL 2 * Exercici 1 * * * </h3>
+    <p>Imagina que tens dues llistes de convidats(representats/es únicament per noms). Fes un programa que et retorni:</p>
+    <ol>
+        <li>La llista de convidats en comú entre les dues llistes.</li>
+        <li>La mescla de la llista de convidats(sense repeticions).</li>
+        <li>La llista de convidats exclusius de la primera llista.</li>
+        <li>La llista de convidats exclusius de la segona llista.</li>
+    </ol>
 
+    <h4> Creació de llistes:</h4>
 
-#La mescla de la llista de convidats(sense repeticions).
-$newList = array_merge($list1, $list2);
-$newList = array_unique($newList);
-$newList = array_values($newList);
-echo "Convidats, sense repeticions: " . implode(", ", $newList) . "\n";
+    <?php
+    $list1 = ["Arale", "Gatchan", "Akane", "Pisuke", "Taro", "Sembei"];
+    $list2 = ["Ranma", "Akane", "Xampu", "Ryoga", "Taro"];
 
+    echo "<strong>Llistat 1:</strong> " . implode(", ", $list1) . "<br>";
+    echo "<strong>Llistat 2:</strong> " . implode(", ", $list2); ?>
 
-#La llista de convidats exclusius de la primera llista.
-$exclusiveList1 = [];
+    <h4>1. La llista de convidats en comú entre les dues llistes.</h4>
 
-foreach ($list1 as $guest) {
-    if (!in_array($guest, $list2)) {
-        $exclusiveList1[] = $guest;
+    <?php $commonGuests = array_intersect($list1, $list2);
+    echo "<em>New list after array_intersect:<br></em>";
+    echo "indexed list:<br>";
+    print_r($commonGuests); ?>
+
+    <h4>2. La mescla de la llista de convidats (sense repeticions)</h4>
+    <?php
+    echo "<em>New list after array_merge:<br></em>";
+    $newList = array_merge($list1, $list2);
+
+    echo "indexed list: <br>";
+    print_r($newList);
+
+    echo "<br><br><em>New list after array_unique:</em><br>";
+    $newList = array_unique($newList);
+
+    echo "indexed list: <br>";
+    echo print_r($newList);
+
+    echo "<br><br>* Convidats, sense repeticions:";
+    echo "<br><em>New list after array_values:</em><br>";
+    $newList = array_values($newList);
+
+    print_r($newList);
+
+    ?>
+    <h4>3. La llista de convidats exclusius de la primera llista.</h4>
+
+    <?php
+    $exclusiveList1 = [];
+
+    foreach ($list1 as $guest) {
+        if (!in_array($guest, $list2)) {
+            $exclusiveList1[] = $guest;
+        }
     }
-}
 
-echo "Convidats exclusius de la llista-1: " . implode(", ", $exclusiveList1) . "\n";
+    echo "* Convidats exclusius de la llista 1: <br>";
+    echo "indexed list: <br>";
+    print_r($exclusiveList1);
+    ?>
 
-#La llista de convidats exclusius de la segona llista.
-$exclusiveList2 = [];
+    <h4>4. La llista de convidats exclusius de la segona llista.</h4>
+    <?php
 
-foreach ($list2 as $guest) {
-    if (!in_array($guest, $list1)) {
-        $exclusiveList2[] = $guest;
+    $exclusiveList2 = [];
+
+    foreach ($list2 as $guest) {
+        if (!in_array($guest, $list1)) {
+            $exclusiveList2[] = $guest;
+        }
     }
-}
 
-echo "Convidats exclusious de la llista-2: " . implode(", ", $exclusiveList2);
+    echo "<br>* Convidats exclusious de la llista 2: <br>";
+    echo "indexed list: <br>";
+    print_r($exclusiveList2);
+    //----------------------------------------------------------------------------------------------
+    ?>
+    <hr>
+    <h4> Creació de llistes:</h4>
+    <?php
+
+    $listA = ["Arale", "Gatchan", "Akane", "Pisuke", "Taro", "Sembei"];
+    $listB = ["Ranma", "Akane", "Xampu", "Ryoga", "Taro"];
+
+    echo "<strong>Llist A:</strong> " . implode(", ", $listA) . "<br>";
+    echo "<strong>Llistat B:</strong> " . implode(", ", $listB); ?>
+
+    <h4>1. La llista de convidats en comú entre les dues llistes.</h4>
+
+    <?php
+    echo "<em>New list after array_intersect:<br></em>";
+    echo "indexed list: <br>";
+    $commonG = array_intersect($listA, $listB);
+    print_r($commonG); ?>
+
+    <h4>2. La mescla de la llista de convidats (sense repeticions)</h4>
+
+    <?php
+    echo "<em>New list after array_unique(array_merge):<br></em>";
+    echo "indexed list: <br>";
+    $all = array_unique(array_merge($listA, $listB));
+    print_r($all); ?>
+
+    <h4>3. La llista de convidats exclusius de la primera llista.</h4>
+    <?php
+    echo "<em>New list after array_diff<br></em>";
+    echo "indexed list: <br>";
+    $listAexclusive = array_diff($listA, $listB);
+    print_r($listA);
+    ?>
+    <h4>3. La llista de convidats exclusius de la primera llista.</h4>
+
+    <?php
+    echo "<em>New list after array_diff<br></em>";
+    echo "indexed list: <br>";
+    $listBexclusive = array_diff($listB, $listA);
+    print_r($listB);
+    ?>
+
+
+
+</body>
+
+</html>
--- T3-nivell2-ex2.php
+++ T3-nivell2-ex2.php
@@ -1,37 +1,62 @@
-<?php
-echo "<h3> * * *TASCA-3 * NIVELL 2 * Exercici 2 * * * <br></h3>\n";
+<!DOCTYPE html>
+<html lang="en">
 
-/* Crea un programa que llisti les notes dels/les alumnes d’una classe. 
-Per això haurem d’utilitzar un array associatiu on la clau serà el nom de cada alumne. 
-Cada alumne tindrà 5 notes (valorades del 0 al 10).
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>T3-nivell3-ex2</title>
+</head>
 
-A més, crea una funció que, donades les notes de tots els/les alumnes, ens mostri tant 
-la mitjana de la nota de cada alumne, com 
-la nota mitjana de la classe sencera.*/
+<body>
 
-$studentRoster = array(
-    "John" => array(4, 5, 6, 4, 7),
-    "Paul" => array(6, 7, 9, 7, 8),
-    "George" => array(3, 4, 3, 5, 3),
-    "Ringo" => array(4, 6, 7, 9, 8)
-);
 
-const ALL_GRADES = 5;
 
-function calcAverageGrade(array $array)
-{
-    $ClassAverage = 0;
+    <h3> * * *TASCA-3 * NIVELL 2 * Exercici 2 * * * </h3>
 
-    foreach ($array as $student => $grade) {
-        $totalSumofGrades = array_sum($grade);
-        $studentAverage = $totalSumofGrades / ALL_GRADES;
+    <p>Crea un programa que llisti les notes dels/les alumnes d’una classe.
+        Per això haurem d’utilitzar un array associatiu on la clau serà el nom de cada alumne.
+        Cada alumne tindrà 5 notes (valorades del 0 al 10).</p>
 
-        echo "The average grade of $student is $studentAverage." . PHP_EOL;
+    <p>A més, crea una funció que, donades les notes de tots els/les alumnes, ens mostri tant
+        la mitjana de la nota de cada alumne, com
+        la nota mitjana de la classe sencera.</p>
 
-        $ClassAverage += $studentAverage;
+    <h4>the Beatles Class</h4>
+
+    <?php
+
+    const ALL_GRADES = 5;
+
+    $studentRoster = array(
+        "John" => array(4, 5, 6, 4, 7),
+        "Paul" => array(6, 7, 9, 7, 8),
+        "George" => array(3, 4, 3, 5, 3),
+        "Ringo" => array(4, 6, 7, 9, 8)
+    );
+
+    foreach ($studentRoster as $s => $g) {
+        echo "Grades of $s are: " . implode(", ", $g) . "<br>";
     }
 
-    echo "The class average is $ClassAverage .";
-}
+    echo "<br>";
+    function calcAverageGrade(array $array)
+    {
+        $ClassAverage = 0;
 
-calcAverageGrade($studentRoster);
+        foreach ($array as $student => $grade) {
+            $totalSumofGrades = array_sum($grade);
+            $studentAverage = $totalSumofGrades / ALL_GRADES;
+
+            echo "The average grade of $student is $studentAverage <br>";
+
+            $ClassAverage += $studentAverage;
+        }
+
+        $ClassAverage = $ClassAverage / ALL_GRADES;
+        echo "The class average is $ClassAverage .";
+    }
+
+    calcAverageGrade($studentRoster); ?>
+</body>
+
+</html>
